6a30c9aee3eb5d030fc6d560511a5fc613cd41c9
Roman Melnychuk <jenikuse@gmail.com>
2019-07-07 03:10:58 +0300

    deleted time.sleep() method

--- test_items.py
+++ test_items.py
@@ -1,4 +1,3 @@
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,6 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket_exists(browser):
     browser.get(link)
-    time.sleep(5)
     btn_add_to_basket = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
     assert btn_add_to_basket != None 
